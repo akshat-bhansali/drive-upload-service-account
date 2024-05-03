decdad183b9b607e32f1f5dcb225801c5f6b8307
akshatbhansali <akshatb2198@gmail.com>
2024-05-04 02:00:31 +0530

    removed fs and added stream for buffer

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-import { google } from 'googleapis';
-import fs from 'fs';
+const { google } = require("googleapis");
+const stream = require("stream");
 
-
-export async function fileUploader(keyFileLocation,parentFileName,files){
-    try{
-        const auth = new google.auth.GoogleAuth ({
-            keyFile: keyFileLocation,
-            scopes: ["https://www.googleapis.com/auth/drive"],
-            });
-            const drive = google. drive ({
-                version: "v3", auth,
-                });
-                const uploadedFiles = []
-                for(let i=0;i<files.length;i++){
-                    const file = files[i];
-                    const response = await drive.files.create({
-                        requestBody: {
-                            name: file.originalname, 
-                            mimeType: file.mimetype,
-                            parents: [parentFileName],
-                        },
-                        media: {
-                            body: fs.createReadStream(file.path),
-                        },
-                    })
-                    uploadedFiles.push(response.data);
-                }
-                // res.json({ files: uploadedFiles });
-                return(uploadedFiles);
-    }catch (e){
-        console.log(e);
-        throw e;
+export async function fileUploader(keyFileLocation, parentFileName, files) {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: keyFileLocation,
+      scopes: ["https://www.googleapis.com/auth/drive"],
+    });
+    const drive = google.drive({
+      version: "v3",
+      auth,
+    });
+    const uploadedFiles = [];
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const bufferStream = new stream.PassThrough();
+      bufferStream.end(file.buffer);
+      const response = await drive.files.create({
+        requestBody: {
+          name: file.originalname,
+          mimeType: file.mimetype,
+          parents: [parentFileName],
+        },
+        media: {
+          body: bufferStream,
+        },
+      });
+      const fileUrl = `https://drive.google.com/file/d/${response.data.id}/view?usp=sharing`;
+      uploadedFiles.push({ ...response.data, url: fileUrl });
     }
+    return uploadedFiles;
+  } catch (e) {
+    throw e;
+  }
 }
